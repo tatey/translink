a2f9bc3172b072697ffc3b67667b04b491124a0e
Tate Johnson <tate@tatey.com>
2011-11-20 22:00:00 +1000

    Timetable has routes

--- lib/translink/pages/timetable.rb
+++ lib/translink/pages/timetable.rb
@@ -1,18 +1,24 @@
 module Translink
   module Pages
     class Timetable
-      attr_reader :page, :url
+      attr_reader :page, :routes, :url
       
       def initialize url
         @url = url
       end
       
-      def anchors
-        page.search 'table tr td:first-child a'
-      end
-      
       def page
         @page ||= Mechanize.new.get url
+      end
+      
+      def routes
+        @routes ||= anchors.map { |anchor| Route.new anchor['href'] }
+      end
+      
+    protected
+    
+      def anchors
+        page.search 'table tr td:first-child a'
       end
     end
   end
--- test/translink/pages/timetable_test.rb
+++ test/translink/pages/timetable_test.rb
@@ -1,23 +1,21 @@
 require 'helper'
 
 class Pages::TimetableTest < MiniTest::Unit::TestCase
-  def test_initialization_writes_attributes    
+  def test_initialize 
     assert_equal 'http://localhost', Pages::Timetable.new('http://localhost').url
   end
   
-  def test_page_gets_url
+  def test_page
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
-      to_return(:status => 200, :body => fixture('timetables.html'), :headers => {'Content-Type' => 'text/html'})          
+      to_return(:status => 200, :body => fixture('timetable.html'), :headers => {'Content-Type' => 'text/html'})          
     assert_instance_of Mechanize::Page, Pages::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').page
   end
   
-  def test_anchors_searces_for_routes
+  def test_routes
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
-      to_return(:status => 200, :body => fixture('timetables.html'), :headers => {'Content-Type' => 'text/html'})          
-    anchors = Pages::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').anchors
-    assert_equal '/travel-information/services-and-timetables/buses/view-bus-timetable/1953?timetableDate=2011-11-14&direction=Outbound&routeCode=10', anchors[0][:href]
-    assert_equal '10 - Outbound', anchors[0].text
-    assert_equal '/travel-information/services-and-timetables/buses/view-bus-timetable/1953?timetableDate=2011-11-14&direction=Inbound&routeCode=10', anchors[1][:href]
-    assert_equal '10 - Inbound', anchors[1].text
+      to_return(:status => 200, :body => fixture('timetable.html'), :headers => {'Content-Type' => 'text/html'})          
+    routes = Pages::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').routes
+    assert_equal '/travel-information/services-and-timetables/buses/view-bus-timetable/1953?timetableDate=2011-11-14&direction=Outbound&routeCode=10', routes.first.url
+    assert_equal '/travel-information/services-and-timetables/buses/view-bus-timetable/2503?timetableDate=2011-11-14&direction=Inbound&routeCode=TX5', routes.last.url
   end
 end
