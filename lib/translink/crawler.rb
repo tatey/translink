2105ea1ef7bd280247aa7bea8934d3b3aea51078
Tate Johnson <tate@tatey.com>
2011-11-23 08:34:43 +1000

    Swap in or out the model class for the crawler. Smaller fixtures for quicker tests

--- lib/translink/crawler.rb
+++ lib/translink/crawler.rb
@@ -1,22 +1,24 @@
 module Translink
   class Crawler
-    attr_reader :url
+    attr_accessor :model_class
+    attr_reader   :url
     
     def initialize url
-      @url = URI.parse url
+      @model_class = Model::Route
+      @url         = URI.parse url
     end
     
     def crawl
-      Page::Timetable.new(url.to_s).route_pages.each do |route_page|
-        route_model = route_model_class.find_or_add route_page
-        route_model.add_services route_page.service_models
+      route_pages.each do |route_page|
+        model_instance = model_class.find_or_add route_page
+        model_instance.add_services route_page.service_models
       end
     end
     
   protected
-    
-    def route_model_class
-      @route_model_class ||= Model::Route
+  
+    def route_pages
+      Page::Timetable.new(url.to_s).route_pages
     end
   end
 end
--- test/translink/crawler_test.rb
+++ test/translink/crawler_test.rb
@@ -1,32 +1,45 @@
 require 'helper'
 
 class CrawlerTest < MiniTest::Unit::TestCase
+  class Model
+    attr_reader :services
+
+    def initialize
+      @services = []
+    end
+
+    def add_services services
+      @services += services
+    end
+
+    def self.find_or_add route_page
+      instance
+    end
+    
+    def self.instance
+      @instance ||= new
+    end
+  end
+  
   def test_initialize  
     assert_equal 'http://localhost', Crawler.new('http://localhost').url.to_s
   end
   
   def test_crawl
-    skip 'This test is way too slow.'
-    
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
-      to_return(:status => 200, :body => fixture('timetable.html'), :headers => {'Content-Type' => 'text/html'})          
+      to_return(:status => 200, :body => fixture('small_timetable.html'), :headers => {'Content-Type' => 'text/html'})          
     stub_request(:get, /http:\/\/jp.translink.com.au\/travel-information\/services-and-timetables\/buses\/view-bus-timetable\/\d+\?direction=(In|Out)bound&routeCode=[0-9A-Z]+&timetableDate=\d{4}-\d{2}-\d{2}/).
-      to_return(:status => 200, :body => fixture('route.html'), :headers => {'Content-Type' => 'text/html'})
+      to_return(:status => 200, :body => fixture('small_route.html'), :headers => {'Content-Type' => 'text/html'})
     stub_request(:get, /http:\/\/jp.translink.com.au\/travel-information\/services-and-timetables\/trip-details\/\d+\?timetableDate=\d{4}-\d{2}-\d{2}/).
       to_return(:status => 200, :body => fixture('trip.html'), :headers => {'Content-Type' => 'text/html'})
-    
-    service_models = []
-    route_model_instance = MiniTest::Mock.new
-    route_model_instance.expect :add_services, true, [Array]
-    route_model_instance.expect :services, service_models
-    route_model_class = MiniTest::Mock.new
-    route_model_class.expect :find_or_add, route_model_instance, [Page::Route]
-    
     crawler = Crawler.new 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables'
-    crawler.instance_variable_set '@route_model_class', route_model_class
+    crawler.model_class = Model
     crawler.crawl
+    assert_equal 150, Model.instance.services.size
     
-    assert_equal 20, service_models.size
-    skip 'Test first and last match the expected times. Use a datetime object.'
+    skip
+    
+    assert_equal '', Model.instance.services.first.time
+    assert_equal '', Model.instance.services.last.time
   end
 end
