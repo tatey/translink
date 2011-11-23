7bd35d0f53190c064910fb13f708375002f501a1
Tate Johnson <tate@tatey.com>
2011-11-23 21:48:46 +1000

    Return DateTime objects

--- lib/translink/page/trip.rb
+++ lib/translink/page/trip.rb
@@ -6,13 +6,17 @@ module Translink
       def initialize url
         @url = url
       end
+      
+      def date
+        Date.parse page.search('div#contentleftCol p span').text
+      end
 
       def stops
         table_rows.search('td:first-child').map { |td| td.text.strip }
       end
       
       def times
-        table_rows.search('td:last-child').map { |td| td.text.strip }
+        table_rows.search('td:last-child').map { |td| date_time(td.text.strip) }
       end
       
       def page
@@ -20,6 +24,10 @@ module Translink
       end
       
     protected
+    
+      def date_time time_string
+        DateTime.parse "#{date} #{time_string.sub('.', ':')} +1000"
+      end
     
       def table_rows
         @table_rows ||= page.search 'tbody tr'
--- test/translink/page/trip_test.rb
+++ test/translink/page/trip_test.rb
@@ -5,19 +5,25 @@ class Page::TripTest < MiniTest::Unit::TestCase
     assert_equal 'http://localhost', Page::Trip.new('http://localhost').url.to_s
   end
   
-  def test_stops
+  def test_date
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').
       to_return(:status => 200, :body => fixture('verbatim/trip.html'), :headers => {'Content-Type' => 'text/html'})
-    times = Page::Trip.new('http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').times
-    assert_equal '5.00am', times.first
-    assert_equal '5.50am', times.last
+    assert_equal Date.parse('Monday 14 November 2011'), Page::Trip.new('http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').date
   end
   
-  def test_times
+  def test_stops
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').
       to_return(:status => 200, :body => fixture('verbatim/trip.html'), :headers => {'Content-Type' => 'text/html'})
     stops = Page::Trip.new('http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').stops
     assert_equal "Illaweena St (Waterstone)\nWaterstone, Illaweena St far side of Waterbrooke Crt", stops.first
     assert_equal "Queen St Bus Station A6\nQsbs Stop A6", stops.last
+  end
+  
+  def test_times
+    stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').
+      to_return(:status => 200, :body => fixture('verbatim/trip.html'), :headers => {'Content-Type' => 'text/html'})
+    times = Page::Trip.new('http://jp.translink.com.au/travel-information/services-and-timetables/trip-details/281889?timetableDate=2011-11-14').times
+    assert_equal DateTime.parse('2011-11-14 05:00 +1000'), times.first
+    assert_equal DateTime.parse('2011-11-14 05:50 +1000'), times.last
   end
 end
