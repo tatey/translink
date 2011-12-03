aa10c691128de99e5ba508421016d52690a3a74e
Tate Johnson <tate@tatey.com>
2011-12-03 18:41:56 +1000

    Real data

--- test/translink/model/stop_test.rb
+++ test/translink/model/stop_test.rb
@@ -4,8 +4,8 @@ class Model::StopTest < MiniTest::Unit::TestCase
   def test_find_or_add_from_stop
     DB.new('sqlite::memory:').use do 
       stop = MiniTest::Mock.new
-      stop.expect :name, '130'
-      stop.expect :locality, 'City Buz 130 Via Sunnybank'
+      stop.expect :name, 'Illaweena St (Waterstone)'
+      stop.expect :locality, 'Waterstone, Illaweena St far side of Waterbrooke Crt'
       assert_equal Model::Stop.find_or_add_from_stop(stop), Model::Stop.find_or_add_from_stop(stop)
       assert_equal 1, Model::Stop.count
     end
