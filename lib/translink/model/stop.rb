efd7def3571e7e993b7a1173b04754be1fd4c69c
Tate Johnson <tate@tatey.com>
2012-03-13 21:43:56 +1000

    Extract street1, street2 and locality from summary or name.

--- lib/translink/model/stop.rb
+++ lib/translink/model/stop.rb
@@ -2,10 +2,22 @@ module Translink
   module Model
     class Stop
       include DataMapper::Resource
-      
+
+      REGEXES = {
+        /[\s\(]opposite approaching/i => 'opposite_approaching',
+        /[\s\(]opposite far side of/i => 'opposite_far_side',
+        /[\s\(]opposite/i             => 'opposite',
+        /[\s\(]approaching/i          => 'approaching',
+        /[\s\(]far side of/i          => 'far_side',
+        /[\s\(]at/i                   => 'at',
+        /[\s\(]near/i                 => 'near'
+      }
+
       property :id,       Serial
       property :name,     String
       property :summary,  String
+      property :street1,  String
+      property :street2,  String
       property :locality, String
       
       has n, :services    
@@ -13,6 +25,42 @@ module Translink
       
       def self.find_or_add_from_stop stop
         Stop.first_or_create :name => stop.name, :summary => stop.summary
+      end
+
+      def extract_from_summary_or_name!
+        segments = summary_or_name.split regex
+        if segments.size == 2
+          self.street1  = sanitize_street1 segments[0]
+          self.street2  = sanitize_street2 segments[1]
+          self.locality = REGEXES[regex]
+        else
+          self.street1  = summary_or_name.strip
+          self.street2  = nil
+          self.locality = nil
+        end
+      end
+
+    protected
+
+      def regex
+        REGEXES.keys.find { |regex| summary_or_name =~ regex }
+      end
+
+      def sanitize_street1 street
+        match_data = street.match /,\s*(.+)/
+        if match_data && match_data[1]
+          match_data[1].strip
+        else
+          street.strip
+        end
+      end
+
+      def sanitize_street2 street
+        street.gsub(/[\(\)]/, '').strip
+      end
+
+      def summary_or_name
+        summary ? summary : name
       end
     end
   end
--- test/translink/model/stop_test.rb
+++ test/translink/model/stop_test.rb
@@ -10,4 +10,42 @@ class Model::StopTest < MiniTest::Unit::TestCase
       assert_equal 1, Model::Stop.count
     end
   end
+
+  def test_extract_from_summary_or_name_with_bang_sets_street1_street2_and_locality
+    s1 = Model::Stop.new :summary => 'Grand Ave East, Grand Av opposite approaching Ascot Av'
+    s1.extract_from_summary_or_name!
+    assert_equal 'Grand Av', s1.street1
+    assert_equal 'Ascot Av', s1.street2
+    assert_equal 'opposite_approaching', s1.locality
+    s2 = Model::Stop.new :summary => 'P.A. Hospital - 14, Ipswich Rd opposite far side of Tottenham St'
+    s2.extract_from_summary_or_name!
+    assert_equal 'Ipswich Rd', s2.street1
+    assert_equal 'Tottenham St', s2.street2
+    assert_equal 'opposite_far_side', s2.locality
+    s3 = Model::Stop.new :summary => 'Partridge, Partridge St opposite Tang St'
+    s3.extract_from_summary_or_name!
+    assert_equal 'Partridge St', s3.street1
+    assert_equal 'Tang St', s3.street2
+    assert_equal 'opposite', s3.locality
+    s4 = Model::Stop.new :summary => 'Stop 66, Blunder Rd (approaching Ipswich Rd)'
+    s4.extract_from_summary_or_name!
+    assert_equal 'Blunder Rd', s4.street1
+    assert_equal 'Ipswich Rd', s4.street2
+    assert_equal 'approaching', s4.locality
+    s5 = Model::Stop.new :summary => 'Waterstone, Illaweena St far side of Waterbrooke Crt'
+    s5.extract_from_summary_or_name!
+    assert_equal 'Illaweena St', s5.street1
+    assert_equal 'Waterbrooke Crt', s5.street2
+    assert_equal 'far_side', s5.locality
+    s6 = Model::Stop.new :summary => 'Woolloongabba Platform 1, South East Busway At Main St'
+    s6.extract_from_summary_or_name!
+    assert_equal 'South East Busway', s6.street1
+    assert_equal 'Main St', s6.street2
+    assert_equal 'at', s6.locality
+    s7 = Model::Stop.new :summary => 'Allamanda, Old Northern Rd (Near Allamanda Cres)'
+    s7.extract_from_summary_or_name!
+    assert_equal 'Old Northern Rd', s7.street1
+    assert_equal 'Allamanda Cres', s7.street2
+    assert_equal 'near', s7.locality
+  end
 end
