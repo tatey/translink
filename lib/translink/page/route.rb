a089b2d33054f94ebbcef30b3bbb9c6b5f257343
Tate Johnson <tate@tatey.com>
2012-04-08 19:23:53 +1000

    Whitespace.

--- lib/translink/page/route.rb
+++ lib/translink/page/route.rb
@@ -3,23 +3,23 @@ module Translink
     def code
       page.search('div#contentleftCol table th:nth-child(2)').first.text.strip
     end
-    
+
     def date
       Date.parse page.search('div#contentleftCol div.content p span').text
     end
-          
+
     def direction
       page.search('div#contentleftCol div.content p').text.downcase.match(/(\S+)$/).captures.first
     end
-    
+
     def name
       page.search('div#headingBar h1').text
     end
-    
+
     def translink_id
       url.path.match(/([^\/]+)$/).captures.last
     end
-    
+
     def trip_pages
       page.search('table:not(:last-child) tfoot a').map do |anchor|
         Trip.new url_from_href(anchor[:href])
--- test/unit/page/route_test.rb
+++ test/unit/page/route_test.rb
@@ -6,31 +6,31 @@ class Page::RouteTest < MiniTest::Unit::TestCase
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
     assert_equal '130', Page::Route.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').code
   end
-  
+
   def test_date
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
     assert_equal Date.parse('Monday 14 November 2011'), Page::Route.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').date
   end
-  
+
   def test_direction
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
     assert_equal 'inbound', Page::Route.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').direction
   end
-  
+
   def test_name
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
     assert_equal 'City Buz 130 Via Sunnybank', Page::Route.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').name
   end
-  
+
   def test_translink_id
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
     assert_equal '1925', Page::Route.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').translink_id
   end
-  
+
   def test_trip_pages
     stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/1925?timetableDate=2011-11-14&direction=Inbound&routeCode=130').
       to_return(:status => 200, :body => fixture('verbatim/route.html'), :headers => {'Content-Type' => 'text/html'})
