8b0a1082f5cd6913b0bba02dbf6c06ad870a73c6
Tate Johnson <tate@tatey.com>
2012-06-01 21:23:03 +1000

    New timetable page.
    * Routes are no longer listed with direction from the timetable page.
    * Added documentation.

--- lib/translink/page/timetable.rb
+++ lib/translink/page/timetable.rb
@@ -6,26 +6,29 @@ module Translink
       @code_class ||= Code
     end
 
+    # Builds an array of routes.
+    #
+    # @return [Array(Route)]
     def route_pages
-      page.search('table tr td:first-child a').reduce Array.new do |pages, anchor|
-        route = Route.new url_from_href anchor['href']
-        pages << route if code_class.brisbane? extract_code_from_anchor(anchor)
+      page.search('table tr td:last-child a').reduce Array.new do |pages, anchor|
+        route = Route.new url_from_href(anchor['href'])
+        pages << route
         pages
       end
     end
 
-    def timetable_page date
-      form = page.forms[1]
-      form.field_with(:name => 'TimetableDate').value = date.to_s
+    # Returns a timetable page with routes running on the given date.
+    #
+    # @param timestamp [DateTime] Filter by this date.
+    #   TZ should be Australia/Brisbane and the time should be at midnight.
+    # @return [Timetable]
+    def timetable_page timestamp
+      form  = page.forms[0]
+      value = timestamp.strftime('%-d/%m/%y %I:%M:%S %p') # Eg: "4/06/2012 12:00:00 AM"
+      form.field_with(:name => 'Date').value = value
       self.class.new(url_from_href(form.action)).tap do |page|
         page.page = form.submit
       end
-    end
-
-  protected
-
-    def extract_code_from_anchor anchor
-      anchor.text.gsub(/\-\s(Inbound|Outbound)/, '').strip
     end
   end
 end
--- test/unit/page/timetable_test.rb
+++ test/unit/page/timetable_test.rb
@@ -2,25 +2,25 @@ require 'helper'
 
 class Page::TimetableTest < MiniTest::Unit::TestCase
   def test_page
-    stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
+    stub_request(:get, 'http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').
       to_return(:status => 200, :body => fixture('verbatim/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    assert_instance_of Mechanize::Page, Page::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').page
+    assert_instance_of Mechanize::Page, Page::Timetable.new('http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').page
   end
 
   def test_route_pages
-    stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
+    stub_request(:get, 'http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').
       to_return(:status => 200, :body => fixture('verbatim/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    route_pages = Page::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').route_pages
-    assert_equal 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/2245?timetableDate=2011-11-14&direction=Outbound&routeCode=100', route_pages.first.url.to_s
-    assert_equal 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/view-bus-timetable/2631?timetableDate=2011-11-14&direction=Inbound&routeCode=LOOP', route_pages.last.url.to_s
+    route_pages = Page::Timetable.new('http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').route_pages
+    assert_equal 'http://jp.translink.com.au/travel-information/network-information/buses/3', route_pages.first.url.to_s
+    assert_equal 'http://jp.translink.com.au/travel-information/network-information/buses/TX5', route_pages.last.url.to_s
   end
 
   def test_timetable_page
-    stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables').
+    stub_request(:get, 'http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').
       to_return(:status => 200, :body => fixture('verbatim/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    stub_request(:post, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
+    stub_request(:post, 'http://jp.translink.com.au/travel-information/network-information/timetables').
       to_return(:status => 200, :body => fixture('verbatim/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    timetable_page = Page::Timetable.new('http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables').timetable_page Date.parse('2011-11-27')
-    assert_equal 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables', timetable_page.url.to_s
+    timetable_page = Page::Timetable.new('http://jp.translink.com.au/travel-information/network-information/buses/all-timetables').timetable_page DateTime.parse('2012-06-04')
+    assert_equal 'http://jp.translink.com.au/travel-information/network-information/timetables', timetable_page.url.to_s
   end
 end
