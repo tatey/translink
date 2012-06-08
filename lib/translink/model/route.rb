8f9b2acda7ceae1fcd78e36b08620f5fafe6005c
Tate Johnson <tate@tatey.com>
2012-06-08 23:59:32 +1000

    Changed properties of Model::Route.

--- lib/translink/model/route.rb
+++ lib/translink/model/route.rb
@@ -5,20 +5,24 @@ module Translink
 
       storage_names[:default] = 'routes'
 
-      property :id,           Serial
-      property :code,         String
-      property :name,         String
-      property :direction,    String
-      property :translink_id, Integer
+      property :id,         Serial
+      property :route_id,   Integer # Unique ID assigned by Translink.
+      property :short_name, String  # Route code. Eg "130".
+      property :long_name,  String  # Suburbs serviced or destination. Eg "City, Sunnybank, Algester". 
+      property :route_type, Integer # Type of transporation. Eg "Bus".
 
-      has n, :services
-      has n, :stops, :through => :services
+      has n, :trips
 
+      # Route model for the given +route_page+. Will create the route if it
+      # doesn't exist.
+      #
+      # @param route_pate [Page::Route] HTML page that represents the route.
+      # @return [Model::Route]
       def self.find_or_add_from_route_page route_page
-        first_or_create :code         => route_page.code,
-                        :name         => route_page.name,
-                        :direction    => route_page.direction,
-                        :translink_id => route_page.translink_id
+        first_or_create :route_id   => route_page.route_id,
+                        :short_name => route_page.short_name,
+                        :long_name  => route_page.long_name,
+                        :route_type => route_page.route_type
       end
     end
   end
--- test/unit/model/route_test.rb
+++ test/unit/model/route_test.rb
@@ -4,10 +4,10 @@ class Model::RouteTest < MiniTest::Unit::TestCase
   def test_find_or_add_from_route_page
     DB.context 'sqlite::memory:', :migrate => true do
       route_page = MiniTest::Mock.new
-      route_page.expect :code, '130'
-      route_page.expect :name, 'City Buz 130 Via Sunnybank'
-      route_page.expect :direction, 'inbound'
-      route_page.expect :translink_id, 1
+      route_page.expect :route_id, 130
+      route_page.expect :short_name, '130'
+      route_page.expect :long_name, 'City, Griffith Uni, Sunnybank Hills, Algester, Parkinson'
+      route_page.expect :route_type, 0
       assert_equal Model::Route.find_or_add_from_route_page(route_page), Model::Route.find_or_add_from_route_page(route_page)
       assert_equal 1, Model::Route.count
     end
