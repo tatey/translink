54e5df482a7f2feb58a3737808cfbd0d3c414447
Tate Johnson <tate@tatey.com>
2012-06-08 23:58:53 +1000

    Remove add_service_from_trip_page from Model::Route.

--- lib/translink/model/route.rb
+++ lib/translink/model/route.rb
@@ -14,13 +14,6 @@ module Translink
       has n, :services
       has n, :stops, :through => :services
 
-      def add_service_from_trip_page trip_page
-        trip_page.trips.each do |trip|
-          services << Service.build_from_trip(trip)
-          services.last.save
-        end
-      end
-
       def self.find_or_add_from_route_page route_page
         first_or_create :code         => route_page.code,
                         :name         => route_page.name,
--- test/unit/model/route_test.rb
+++ test/unit/model/route_test.rb
@@ -1,28 +1,6 @@
 require 'helper'
 
 class Model::RouteTest < MiniTest::Unit::TestCase
-  def test_add_services_from_trip_pages
-    DB.context 'sqlite::memory:', :migrate => true do
-      trip = Page::Trip::Trip.new.tap do |t|
-        t.stop = Page::Trip::Stop.new 'Illaweena St (Waterstone)', 'Waterstone, Illaweena St far side of Waterbrooke Crt'
-        t.time = DateTime.parse '2011-11-24 23:17:00'
-      end
-      route_page = MiniTest::Mock.new
-      route_page.expect :code, '130'
-      route_page.expect :name, 'City Buz 130 Via Sunnybank'
-      route_page.expect :direction, 'inbound'
-      route_page.expect :translink_id, 1
-      trip_page = MiniTest::Mock.new
-      trip_page.expect :trips, [trip]
-      route_model = Model::Route.find_or_add_from_route_page route_page
-      route_model.add_service_from_trip_page trip_page
-      route_model.add_service_from_trip_page trip_page
-      route_model.add_service_from_trip_page trip_page
-      assert_equal 3, Model::Service.count
-      assert_equal 1, Model::Stop.count
-    end
-  end
-
   def test_find_or_add_from_route_page
     DB.context 'sqlite::memory:', :migrate => true do
       route_page = MiniTest::Mock.new
