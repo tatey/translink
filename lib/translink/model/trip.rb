a0e9c73c245eb50b9b7063c544eaf3f2b8ba4e0f
Tate Johnson <tate@tatey.com>
2012-06-15 23:35:18 +1000

    Trip model can set properties from trip page.

--- lib/translink/model/trip.rb
+++ lib/translink/model/trip.rb
@@ -11,6 +11,16 @@ module Translink
       property :trip_id,    Integer
 
       belongs_to :route
+
+      # Sets properties from the given +trip_page+. 
+      #
+      # @param trip_page [Trip::Page] HTML page that represents the trip.
+      # @return [void]
+      def trip_page! trip_page
+       self.direction  = trip_page.direction
+       self.service_id = trip_page.service_id
+       self.trip_id    = trip_page.trip_id
+      end
     end
   end
 end
