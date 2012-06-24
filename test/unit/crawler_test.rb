ca093e9b6a8aca7315f84c85604ad71c96181a50
Tate Johnson <tate@tatey.com>
2012-06-24 22:44:20 +1000

    Fix crawler test.

--- test/unit/crawler_test.rb
+++ test/unit/crawler_test.rb
@@ -1,42 +1,22 @@
 require 'helper'
 
 class CrawlerTest < MiniTest::Unit::TestCase
-  class Model
-    attr_reader :trip_pages
-
-    def initialize
-      @trip_pages = []
-    end
-
-    def add_service_from_trip_page trip_pages
-      @trip_pages << trip_pages
-    end
-
-    def self.find_or_add_from_route_page route_page
-      instance
-    end
-
-    def self.instance
-      @instance ||= new
-    end
-  end
-
-  def test_initialize
-    assert_equal 'http://localhost', Crawler.new('http://localhost').url.to_s
-  end
-
   def test_crawl
-    stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables').
-      to_return(:status => 200, :body => fixture('sample/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    stub_request(:get, /http:\/\/jp.translink.com.au\/travel-information\/services-and-timetables\/buses\/view-bus-timetable\/\d+\?direction=(In|Out)bound&routeCode=[0-9A-Z]+&timetableDate=\d{4}-\d{2}-\d{2}/).
-      to_return(:status => 200, :body => fixture('sample/route.html'), :headers => {'Content-Type' => 'text/html'})
-    stub_request(:get, /http:\/\/jp.translink.com.au\/travel-information\/services-and-timetables\/trip-details\/\d+\?timetableDate=\d{4}-\d{2}-\d{2}/).
-      to_return(:status => 200, :body => fixture('verbatim/trip.html'), :headers => {'Content-Type' => 'text/html'})
-    stub_request(:post, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/bus-timetables').
-      to_return(:status => 200, :body => fixture('sample/timetable.html'), :headers => {'Content-Type' => 'text/html'})
-    crawler = Crawler.new 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables'
-    crawler.__model__ = Model
-    crawler.crawl Date.today
-    assert_equal 6, Model.instance.trip_pages.size
+    DB.context 'sqlite::memory:', :migrate => true do
+      stub_request(:get, 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables').
+        to_return(:status => 200, :body => fixture('sample/timetable.html'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:post, 'http://jp.translink.com.au/travel-information/network-information/timetables').
+        to_return(:status => 200, :body => fixture('sample/timetable.html'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:get, %r{http://jp.translink.com.au/travel-information/network-information/buses/\w+}).
+        to_return(:status => 200, :body => fixture('sample/route.html'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:get, %r{http://jp.translink.com.au/travel-information/network-information/service-information/\w+/\d+/\d+/\d+-\d+-\d+}).
+        to_return(:status => 200, :body => fixture('verbatim/trip.html'), :headers => {'Content-Type' => 'text/html'})
+      crawler = Crawler.new 'http://jp.translink.com.au/travel-information/services-and-timetables/buses/all-bus-timetables'
+      crawler.crawl Date.today
+      assert_equal 1, Model::Route.count
+      assert_equal 1, Model::Trip.count
+      assert_equal 27, Model::Stop.count
+      assert_equal 27, Model::StopTime.count
+    end
   end
 end
