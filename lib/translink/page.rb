66f6c318eb7340cad91f1d423398cc2601cfb7d4
Tate Johnson <tate@tatey.com>
2012-09-08 11:37:32 +1000

    Whitespace.

--- lib/translink/page.rb
+++ lib/translink/page.rb
@@ -1,20 +1,20 @@
 module Translink
   class Page
     USER_AGENT = "Mozilla/5.0 (Translink/#{VERSION} Ruby/#{RUBY_VERSION} (https://github.com/tatey/translink))"
-    
+
     attr_accessor :agent, :page, :url
-    
+
     def initialize url
       @agent = Mechanize.new.tap { |mechanize| mechanize.user_agent = USER_AGENT }
       @url   = URI.parse url
     end
-        
+
     def page
       @page ||= agent.get url.to_s
     end
-    
+
   protected
-  
+
     def url_from_href href
       url.scheme + '://' + url.host + href
     end
