e7f3f3cd4c55642b8d592c21dbf17aabe0b94828
Tate Johnson <tate@tatey.com>
2012-09-23 19:32:09 +1000

    Enforce foreign key constraints.

--- lib/translink/db.rb
+++ lib/translink/db.rb
@@ -4,6 +4,9 @@ module Translink
       DataMapper.setup :default, uri
       DataMapper.repository :default do |repository|
         DataMapper.finalize
+        repository.adapter.execute <<-SQL
+          PRAGMA foreign_keys=ON;
+        SQL
         if options[:migrate]
           repository.adapter.execute <<-SQL
             DROP TABLE IF EXISTS "routes";
@@ -19,7 +22,8 @@ module Translink
               "trip_id" INTEGER NOT NULL PRIMARY KEY UNIQUE,
               "direction" INTEGER NOT NULL,
               "headsign" TEXT NOT NULL,
-              "route_id" TEXT NOT NULL
+              "route_id" TEXT NOT NULL,
+              FOREIGN KEY ("route_id") REFERENCES "routes" ("route_id") ON DELETE CASCADE
             );
 
             CREATE INDEX "index_trips_on_route_id" ON "trips" ("route_id");
@@ -30,7 +34,9 @@ module Translink
               "stop_sequence" INTEGER NOT NULL,
               "stop_id" TEXT NOT NULL,
               "trip_id" INTEGER NOT NULL,
-              PRIMARY KEY ("arrival_time", "stop_id", "trip_id")
+              PRIMARY KEY ("arrival_time", "stop_id", "trip_id"),
+              FOREIGN KEY ("stop_id") REFERENCES "stops" ("stop_id") ON DELETE RESTRICT,
+              FOREIGN KEY ("trip_id") REFERENCES "trips" ("trip_id") ON DELETE CASCADE
             );
 
             DROP TABLE IF EXISTS "stops";
